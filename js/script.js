3304f6a757b01f33b0783c9d730afaccba9be25c
Anubhav Jain <jainanubhav2003@gmail.com>
2024-09-08 16:05:22 +0530

    updated files

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ const getData = () => {
     if (name != '') {
         [name, "Computer"].forEach((e, i) => names[i].innerText = e);
 
-        layers[0].classList.add("d-none");startGame();
-        setTimeout(() => layers[1].classList.remove("d-none"), 500);
+        startGame();
+        range(2).forEach(e => layers[e].classList.toggle("d-none"));
     }
 }
 
@@ -17,11 +17,78 @@ const startGame = () => {
     rounds.forEach((e, i) => e.innerText = wonRounds[i]);
 }
 
+const clicked = ind => {
+    if (f[ind] != -1) return;
+
+    cells[ind].append(ent[f[ind] = ch ^= 1].content.cloneNode(true));
+    cells[ind].classList.remove("fresh");
+
+    const data = checkWinner();
+
+    if (data != null) {
+        colorCells(data);
+        setTimeout(roundEnd, 300);
+
+        cell_temp_data = data;
+    }
+}
+
+const checkWinner = () => {
+    for (let i = 0; i < 3; i++) {
+        if (f[3 * i] == ch && f[3 * i] == f[3 * i + 1] && f[3 * i] == f[3 * i + 2]) {
+            return [3 * i, 3 * i + 1, 3 * i + 2];
+        } else if (f[i] == ch && f[i] == f[i + 3] && f[i] == f[i + 6]) {
+            return [i, i + 3, i + 6];
+        }
+    }
+
+    if (f[0] == ch && f[0] == f[4] && f[0] == f[8]) {
+        return [0, 4, 8];
+    } else if (f[2] == ch && f[2] == f[4] && f[2] == f[6]) {
+        return [2, 4, 6];
+    }
+
+    return null;
+}
+
+const colorCells = arr => arr.forEach(e => {
+    cells[e].classList.add(`bg-${colors[ch]}`); cells[e].innerHTML = '';
+    cells[e].append(ent[ch + 2].content.cloneNode(true));
+});
+
+const roundEnd = () => {
+    layers[1].classList.add("d-none"); wonRounds[ch] += 1;
+    setTimeout(() => layers[2].classList.remove("d-none"), 1000);
+    score.forEach((e, i) => e.innerText = wonRounds[i]);
+}
+
+const discolorCells = arr => arr.forEach(e => cells[e].classList.remove(`bg-${colors[ch]}`));
+
+const nextRound = () => {
+    range(2).forEach(e => layers[e + 1].classList.toggle("d-none"));
+    cells.forEach(e => {
+        e.classList.add("fresh"); e.innerHTML = '';
+    })
+
+    discolorCells(cell_temp_data);
+    f.fill(-1); ch = 1; startGame();
+}
 
 
 
 
 const layers = document.getElementsByClassName("layer");
+
+const rules = [
+    "Get 3 marks in a row, Player wins.",
+    "Opponent gets 3 in a row, Player loses.",
+    "Board full, no 3 in a row, No winner."
+]
+
+const icons = ["win", "lose", "draw"];
+
+const ruleGrid = document.getElementById("rule-grid");
+const rule_card = document.getElementById("rule-card-template").content;
 
 const cells = [];
 const grid = document.getElementById("grid-container");
@@ -35,9 +102,15 @@ const roundCount = ["1st", "2nd", "3rd", "4th", "5th"];
 const counter = document.getElementById("round");
 
 const wonRounds = [0, 0];
+let cell_temp_data = null;
 
+let ch = 1;
+const f = new Array(9).fill(-1);
+const colors = ["cross", "circle"];
 
-
+const score = new Array();
+const score_card = document.getElementById("score-card");
+const score_holder = document.getElementById("score-holder").content;
 
 
 
@@ -45,7 +118,18 @@ document.addEventListener("keydown", event => {
     if (event.key == "Enter" && !layers[0].classList.contains("d-none")) getData();
 });
 
-(() => { 
+(() => {
+    // range(3).forEach(e => {
+    //     let card = rule_card.cloneNode(true);
+
+    //     card.querySelector(".sm-icon").src = `./img/other/${icons[e]}.png`;
+    //     card.querySelector(".lg-icon").src = `./img/other/${icons[e]} board.png`;
+
+    //     card.querySelector("p").innerText = `${rules[e]} Game ends.`;
+
+    //     ruleGrid.append(card);
+    // });
+
     range(9).forEach(i => {
         let cell = createElem("button", {
             className: "d-grid bg-dark2 br-10 cell fresh",
@@ -66,4 +150,11 @@ document.addEventListener("keydown", event => {
 
         (e == 0) ? cards.prepend(card) : cards.append(card);
     });
+    
+    range(2).forEach(e => {
+        let card = score_holder.cloneNode(true);
+        score.push(card.querySelector("[data-score]"));
+
+        score_card.append(card);
+    })
 })();
