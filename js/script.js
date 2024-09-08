2ed4896c908d8eeade80e94949ffe8cef956c7ad
Anubhav Jain <jainanubhav2003@gmail.com>
2024-09-08 20:40:08 +0530

    updated files

--- js/script.js
+++ js/script.js
@@ -5,33 +5,52 @@ const getData = () => {
 
     if (name != '') {
         [name, "Computer"].forEach((e, i) => names[i].innerText = e);
-
-        counter.innerText = roundCount[round++];
-        rounds.forEach((e, i) => e.innerText = wonRounds[i]);
+        startGame();
 
         range(2).forEach(e => layers[e].classList.toggle("d-none"));
     }
+}
+
+const startGame = () => {
+    counter.innerText = roundCount[round++];
+    rounds.forEach((e, i) => e.innerText = wonRounds[i]);
 }
 
 const clicked = ind => {
     if (f[ind] != -1) return;
 
     cells[ind].append(ent[f[ind] = ch ^= 1].content.cloneNode(true));
-    cells[ind].classList.remove("fresh");
+    cells[ind].classList.remove("fresh"); count++;
 
     data = checkWinner();
 
     if (data != null) {
-        data.forEach(e => {
-            cells[e].classList.add(`bg-${colors[ch]}`); cells[e].innerHTML = '';
-            cells[e].append(ent[ch + 2].content.cloneNode(true));
-        });
+        if (data != "D") {
+            data.forEach(e => {
+                cells[e].classList.add(`bg-${colors[ch]}`); cells[e].innerHTML = '';
+                cells[e].append(ent[ch + 2].content.cloneNode(true));
+            });
+        }
 
         setTimeout((round == 5) ? gameEnd : roundEnd, 300);
     }
+
+    if (ch == 0 && data == null) setTimeout(compChance, 500);
 }
 
-const checkWinner = () => {
+const compChance = () => {
+    let temp = null;
+    console.log("here")
+
+    while (true) {
+        temp = Math.floor(Math.random() * 9);
+        if (f[temp] == -1) break;
+    }
+
+    clicked(temp);
+}
+
+const checkWinner = () => {    
     for (let i = 0; i < 3; i++) {
         if (f[3 * i] == ch && f[3 * i] == f[3 * i + 1] && f[3 * i] == f[3 * i + 2]) {
             return [3 * i, 3 * i + 1, 3 * i + 2];
@@ -46,18 +65,26 @@ const checkWinner = () => {
         return [2, 4, 6];
     }
 
+    if (count == 9) {
+        ch = 2; return "D";
+    }
+
     return null;
 }
 
 const endScreen = () => {
-    layers[1].classList.add("d-none"); wonRounds[ch] += 1;
+    layers[1].classList.add("d-none"); 
+    
+    if (ch != 2) wonRounds[ch] += 1;
+    
     setTimeout(() => layers[2].classList.remove("d-none"), 1000);
     score.forEach((e, i) => e.innerText = wonRounds[i]);
 }
 
 const roundEnd = () => {
     endScreen(); document.getElementById("g-btn").innerText = "Next Round";
-    layers[2].querySelector("#round-text").innerText = (ch) ? "Round Lost" : "Round Won";
+    layers[2].querySelector("#round-text").innerText = 
+    (ch == 2) ? "Round Draw" : (ch == 1) ? "Round Lost" : "Round Won";
 }
 
 const nextRound = () => {
@@ -65,11 +92,11 @@ const nextRound = () => {
     cells.forEach(e => { e.classList.add("fresh"); e.innerHTML = ''; });
 
     if (round == 5) {
-        round = 0; wonRounds = [0, 0];
+        round = -1; wonRounds = [0, 0];
     }
 
-    data.forEach(e => cells[e].classList.remove(`bg-${colors[ch]}`));
-    f.fill(-1); ch = 1;
+    if (data != "D") data.forEach(e => cells[e].classList.remove(`bg-${colors[ch]}`));
+    f.fill(-1); ch = 1; count = 0; data = null; startGame();
 }
 
 const gameEnd = () => {
@@ -110,7 +137,7 @@ const counter = document.getElementById("round");
 let wonRounds = [0, 0];
 let data = null;
 
-let ch = 1;
+let ch = 1, count = 0;
 const f = new Array(9).fill(-1);
 const colors = ["cross", "circle"];
 
